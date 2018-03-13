e317e3fc31f11409cddeda52d4780ca327b8cc75
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-03-13 14:07:39 -0400

    updates from other projects

--- jx_python/jx.py
+++ jx_python/jx.py
@@ -23,7 +23,7 @@ from jx_python import expressions as _expressions
 from jx_python import flat_list, group_by
 from mo_dots import listwrap, wrap, unwrap, FlatList, NullType
 from mo_dots import set_default, Null, Data, split_field, coalesce, join_field
-from mo_future import text_type, boolean_type, none_type, long, generator_types, sort_using_cmp
+from mo_future import text_type, boolean_type, none_type, long, generator_types, sort_using_cmp, PY2
 from mo_logs import Log
 from mo_math import Math
 from mo_math import UNION, MIN
@@ -66,8 +66,8 @@ def run(query, frum=Null):
     BUT IT IS ALSO PROCESSING A list CONTAINER; SEPARATE TO A ListContainer
     """
     if frum == None:
-        query_op = QueryOp.wrap(query)
-        frum = query_op.frum
+        frum = wrap(query)['from']
+        query_op = QueryOp.wrap(query, table=frum, schema=frum.schema)
     else:
         query_op = QueryOp.wrap(query, frum.schema)
 
@@ -89,12 +89,6 @@ def run(query, frum=Null):
     if is_aggs(query_op):
         frum = list_aggs(frum, query_op)
     else:  # SETOP
-        # try:
-        #     if query.filter != None or query.esfilter != None:
-        #         Log.error("use 'where' clause")
-        # except AttributeError:
-        #     pass
-
         if query_op.where is not TRUE:
             frum = filter(frum, query_op.where)
 
@@ -620,7 +614,6 @@ def value_compare(left, right, ordering=1):
 TYPE_ORDER = {
     boolean_type: 0,
     int: 1,
-    long: 1,
     float: 1,
     Date: 1,
     text_type: 2,
@@ -633,9 +626,8 @@ TYPE_ORDER = {
     NullOp: 9
 }
 
-
-
-
+if PY2:
+    TYPE_ORDER[long] = 1
 
 
 def pairwise(values):
