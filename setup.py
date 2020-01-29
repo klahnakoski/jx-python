dc52504bcb52ec28dd5d58237b2bfef39b5f5cde
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-01-28 22:15:23 -0500

    update version number

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ setup(
     long_description_content_type=u'text/markdown',
     include_package_data=True,
     classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 2.7"],
-    install_requires=["mo-collections>=3.5.19316","mo-dots>=3.14.19317","mo-files>=3.4.19316","mo-future>=3.1.19316","mo-json>=3.4.19316","mo-json-config>=3.7.19316","mo-kwargs>=3.5.19316","mo-logs>=3.14.19317","mo-math>=3.7.19316","mo-threads>=3.6.19316","mo-times>=3.4.19316"],
-    version=u'3.15.20029',
+    install_requires=["mo-collections>=3.38.20029","mo-dots>=3.33.20028","mo-files>=3.38.20029","mo-future>=3.32.20028","mo-json>=3.38.20029","mo-json-config>=3.7.19316","mo-kwargs>=3.38.20029","mo-logs>=3.38.20029","mo-math>=3.38.20029","mo-threads>=3.23.19346","mo-times>=3.38.20029"],
+    version=u'3.38.20029',
     url=u'https://github.com/klahnakoski/jx-python',
+    python_requires=u'==2.7, ==3.6, ==3.7, ==3.8',
     packages=["jx_base","jx_base/expressions","jx_python/expressions","jx_python/containers","jx_python/cubes","jx_python/lists","jx_python/namespace","jx_python"],
     long_description=u'# jx-python\nPython library for JSON Expressions \n',
     name=u'jx-python'
