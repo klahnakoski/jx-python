afb0e7fee1429f34c7504c2cabb52a438b9a51e3
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-01-20 16:54:57 -0500

    updates from other projects

--- jx_python/__init__.py
+++ jx_python/__init__.py
@@ -1,11 +1,20 @@
-from mo_threads import stop_main_thread
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
 
+from jx_base import Column
 from jx_python import jx
 from jx_python.containers.cube import Cube
 from jx_python.containers.list import ListContainer
-from jx_python.streams import stream
 from jx_python.expressions import Python
-from jx_base import Column
+from jx_python.streams import stream
+from mo_threads import stop_main_thread
 
 __all__ = ["ListContainer", "Cube", "jx", "stream", "Python", "Column"]
 
