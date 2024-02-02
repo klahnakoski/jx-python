d9fac8d6543154805ab0e33dd2ff99eb324911f3
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-02-01 22:31:01 -0500

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -6,7 +6,7 @@ setup(
     author_email='kyle@lahnakoski.com',
     classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 3.8","Programming Language :: Python :: 3.9","Programming Language :: Python :: 3.11","Programming Language :: Python :: 3.12"],
     description='JSON query expressions using Python',
-    extras_require={"tests":["mo-testing"]},
+    extras_require={"tests":["mo-testing>=7.523.24033"]},
     include_package_data=True,
     install_requires=["mo-collections==5.522.24033","mo-dots==9.520.24032","mo-future==7.520.24032","mo-json==6.522.24033","mo-json-config==4.522.24033","mo-kwargs==7.520.24032","mo-logs==8.522.24033","mo-math==7.521.24032","mo-threads==6.522.24033","mo-times==5.522.24033"],
     license='MPL 2.0',
