f2561b5bcf6254f8f3f4475410dafc904a972fcc
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-02-01 22:22:25 -0500

    fix imports

--- jx_base/__init__.py
+++ jx_base/__init__.py
@@ -15,7 +15,6 @@ from jx_base.expressions.null_op import NULL
 from jx_base.expressions.true_op import TRUE
 from jx_base.expressions.when_op import WhenOp
 from jx_base.language import is_op
-from jx_base.meta_columns import Column
 from jx_base.models.container import Container
 from jx_base.models.facts import Facts
 from jx_base.models.namespace import Namespace
@@ -26,6 +25,7 @@ from jx_base.models.snowflake import Snowflake
 from jx_base.models.table import Table
 from jx_base.utils import enlist
 from jx_python.expressions._utils import Python
+
 
 __all__ = [
     "Container",
@@ -47,3 +47,6 @@ __all__ = [
     "Table",
     "WhenOp",
 ]
+
+# must be last
+from jx_base.meta_columns import Column
