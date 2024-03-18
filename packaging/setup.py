e8a8a6210c099947f8938bc785fb1020eda163ff
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-03-17 22:20:10 -0400

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -8,12 +8,12 @@ setup(
     description='JSON query expressions using Python',
     extras_require={"tests":["mo-testing>=7.562.24075"]},
     include_package_data=True,
-    install_requires=["mo-collections==5.570.24076","mo-dots==9.570.24076","mo-future==7.546.24057","mo-json==6.571.24077","mo-json-config==4.571.24077","mo-kwargs==7.570.24076","mo-logs==8.570.24076","mo-math==7.570.24076","mo-threads==6.570.24076","mo-times==5.570.24076"],
+    install_requires=["mo-collections==5.574.24078","mo-dots==9.574.24078","mo-future==7.546.24057","mo-json==6.574.24078","mo-json-config==4.574.24078","mo-kwargs==7.574.24078","mo-logs==8.574.24078","mo-math==7.574.24078","mo-threads==6.574.24078","mo-times==5.574.24078"],
     license='MPL 2.0',
     long_description='# jx-python\n\nPython library for JSON Expressions \n\n[![PyPI Latest Release](https://img.shields.io/pypi/v/jx-python.svg)](https://pypi.org/project/jx-python/)\n[![Build Status](https://github.com/klahnakoski/jx-python/actions/workflows/build.yml/badge.svg?branch=master)](https://github.com/klahnakoski/jx-python/actions/workflows/build.yml)\n[![Coverage Status](https://coveralls.io/repos/github/klahnakoski/jx-python/badge.svg?branch=dev)](https://coveralls.io/github/klahnakoski/jx-python?branch=dev)\n',
     long_description_content_type='text/markdown',
     name='jx-python',
     packages=["jx_base","jx_base.expressions","jx_base.models","jx_python.expressions","jx_python.containers","jx_python.cubes","jx_python.lists","jx_python.namespace","jx_python","jx_python.streams"],
     url='https://github.com/klahnakoski/jx-python',
-    version='4.572.24077'
+    version='4.574.24078'
 )
