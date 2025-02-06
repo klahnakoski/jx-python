5bdc2eb3aa35975fb6adcf70b423ab3f415e1b53
Kyle Lahnakoski <kyle@lahnakoski.com>
2025-02-05 22:01:44 -0500

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -4,16 +4,16 @@ from setuptools import setup
 setup(
     author='Kyle Lahnakoski',
     author_email='kyle@lahnakoski.com',
-    classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 3.8","Programming Language :: Python :: 3.9","Programming Language :: Python :: 3.11","Programming Language :: Python :: 3.12"],
+    classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 3.8","Programming Language :: Python :: 3.9","Programming Language :: Python :: 3.11","Programming Language :: Python :: 3.12","Programming Language :: Python :: 3.13"],
     description='JSON query expressions using Python',
     extras_require={"tests":["mo-testing>=8.623.24125"]},
     include_package_data=True,
-    install_requires=["mo-collections==5.623.24125","mo-dots==10.623.24125","mo-future==7.584.24095","mo-json==6.624.24125","mo-json-config==4.624.24125","mo-kwargs==7.623.24125","mo-logs==8.623.24125","mo-math==7.623.24125","mo-threads==6.624.24125","mo-times==5.623.24125"],
+    install_requires=["mo-collections==5.672.25036","mo-dots==10.672.25036","mo-future==7.672.25036","mo-json==6.672.25036","mo-json-config==4.673.25037","mo-kwargs==7.672.25036","mo-logs==8.672.25036","mo-math==7.672.25036","mo-threads==6.672.25036","mo-times==5.672.25036"],
     license='MPL 2.0',
     long_description='# jx-python\n\nPython library for JSON Expressions \n\n[![PyPI Latest Release](https://img.shields.io/pypi/v/jx-python.svg)](https://pypi.org/project/jx-python/)\n[![Build Status](https://github.com/klahnakoski/jx-python/actions/workflows/build.yml/badge.svg?branch=master)](https://github.com/klahnakoski/jx-python/actions/workflows/build.yml)\n[![Coverage Status](https://coveralls.io/repos/github/klahnakoski/jx-python/badge.svg?branch=dev)](https://coveralls.io/github/klahnakoski/jx-python?branch=dev)\n',
     long_description_content_type='text/markdown',
     name='jx-python',
     packages=["jx_base","jx_base.expressions","jx_base.models","jx_python.expressions","jx_python.containers","jx_python.cubes","jx_python.lists","jx_python.namespace","jx_python","jx_python.streams"],
     url='https://github.com/klahnakoski/jx-python',
-    version='4.626.24125'
+    version='4.673.25037'
 )
