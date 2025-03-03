678a74f4c84cb3069b91b476052188f2eb606b1f
Kyle Lahnakoski <kyle@lahnakoski.com>
2025-03-02 19:10:25 -0500

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -6,14 +6,14 @@ setup(
     author_email='kyle@lahnakoski.com',
     classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 3.8","Programming Language :: Python :: 3.9","Programming Language :: Python :: 3.11","Programming Language :: Python :: 3.12","Programming Language :: Python :: 3.13"],
     description='JSON query expressions using Python',
-    extras_require={"tests":["mo-testing>=8.623.24125"]},
+    extras_require={"tests":["mo-testing>=8.667.25032"]},
     include_package_data=True,
-    install_requires=["mo-collections==5.672.25036","mo-dots==10.672.25036","mo-future==7.672.25036","mo-json==6.672.25036","mo-json-config==4.673.25037","mo-kwargs==7.672.25036","mo-logs==8.672.25036","mo-math==7.672.25036","mo-threads==6.672.25036","mo-times==5.672.25036"],
+    install_requires=["mo-collections==5.678.25061","mo-dots==10.678.25061","mo-future==7.678.25061","mo-json==6.679.25061","mo-json-config==4.680.25062","mo-kwargs==7.678.25061","mo-logs==8.678.25061","mo-math==7.678.25061","mo-threads==6.679.25061","mo-times==5.678.25061"],
     license='MPL 2.0',
     long_description='# jx-python\n\nPython library for JSON Expressions \n\n[![PyPI Latest Release](https://img.shields.io/pypi/v/jx-python.svg)](https://pypi.org/project/jx-python/)\n[![Build Status](https://github.com/klahnakoski/jx-python/actions/workflows/build.yml/badge.svg?branch=master)](https://github.com/klahnakoski/jx-python/actions/workflows/build.yml)\n[![Coverage Status](https://coveralls.io/repos/github/klahnakoski/jx-python/badge.svg?branch=dev)](https://coveralls.io/github/klahnakoski/jx-python?branch=dev)\n',
     long_description_content_type='text/markdown',
     name='jx-python',
     packages=["jx_base","jx_base.expressions","jx_base.models","jx_python.expressions","jx_python.containers","jx_python.cubes","jx_python.lists","jx_python.namespace","jx_python","jx_python.streams"],
     url='https://github.com/klahnakoski/jx-python',
-    version='4.673.25037'
+    version='4.680.25062'
 )
